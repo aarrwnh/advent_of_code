29a3574e842a44a1584f3b87ea50ab37dc5d0afa
Wenereth Arkhilmor <7783383+aarrwnh@users.noreply.github.com>
2023-11-30 09:39:57 +0900

    slight cleanup

--- 2022/22/solution.py
+++ 2022/22/solution.py
@@ -1,13 +1,7 @@
 import collections
 import re
 
-from support import (
-    Directions,
-    Point,
-    check_result,
-    read_file_raw,
-    timing,
-)
+from support import Directions, Point, check_result, read_file_raw, timing
 
 RE_DIRECTIONS = re.compile(r"([0-9]+)([RL])?")
 # right=0 down=1 left=2 up=3
@@ -127,17 +121,9 @@ def part2(input: str, tile_size: int) -> int:
                         pos = next_pos
                     else:
                         # wrap magic
-                        current_dir_i = (pos.x // tile_size) + (pos.y // tile_size) * 3
-                        if tile_size == 50:
-                            current_tile = [0, 1, 2, 0, 3, 0, 4, 5, 0, 6][current_dir_i]
-                        else:
-                            current_tile = [0, 0, 1, 2, 3, 4, 0, 0, 5, 6][current_dir_i]
-
-                        if current_tile == 0:
-                            raise AssertionError("wrong tile")
-
+                        current_tile_i = (pos.x // tile_size) + (pos.y // tile_size) * 3
                         (_, new_direction, new_x, new_y) = teleporter(
-                            current_tile, direction, pos.x, pos.y, tile_size
+                            current_tile_i, direction, pos.x, pos.y, tile_size
                         )
                         new_pos = Point(new_x, new_y)
                         if new_pos not in blocked:
@@ -148,35 +134,35 @@ def part2(input: str, tile_size: int) -> int:
     return 1000 * (pos.y + 1) + 4 * (pos.x + 1) + direction
 
 
-def teleporter(
-    cur_tile: int, direction: int, x: int, y: int, tile_size: int
-) -> tuple[int, int, int, int]:
-    if tile_size == 50:
-        return resolve_size_50(cur_tile, direction, x, y)
+R = tuple[int, int, int, int]
+
+
+def teleporter(cur_tile_i: int, direction: int, x: int, y: int, tile_size: int) -> R:
+    if tile_size == 50:  # 50x50
+        return resolve_size_50(cur_tile_i, direction, x, y)
     if tile_size == 4:
-        return resolve_size_4(cur_tile, direction, x, y)
+        return resolve_size_4(cur_tile_i, direction, x, y)
     raise AssertionError("tile size not supported")
 
 
-def resolve_size_4(
-    cur_tile: int, direction: int, col: int, row: int
-) -> tuple[int, int, int, int]:
+def resolve_size_4(cur_tile_i: int, direction: int, x: int, y: int) -> R:
+    cur_tile = [0, 0, 1, 2, 3, 4, 0, 0, 5, 6][cur_tile_i]
+    if cur_tile == 0:
+        raise AssertionError("wrong tile")
     match (cur_tile, direction):
         case (1, 0):
-            return 6, 1, 8, row + 3
+            return 6, 1, 8, y + 3
         case (4, 0):
-            return 6, 1, col + 3, 8
+            return 6, 1, x + 3, 8
         case (5, 1):
-            return 2, 3, col - 9, 7
+            return 2, 3, x - 9, 7
         case (3, 3):
-            return 1, 0, 8, col - 4
+            return 1, 0, 8, x - 4
         case _:
             raise AssertionError("unknown wrap")
 
 
-def resolve_size_50(
-    cur_tile: int, direction: int, x: int, y: int
-) -> tuple[int, int, int, int]:
+def resolve_size_50(cur_tile_i: int, direction: int, x: int, y: int) -> R:
     #    ╭─────────╮   ╭─────╮
     #    |       ─────────   |
     #    | ╭─────| 1 | 2 | ╮ |
@@ -188,6 +174,9 @@ def resolve_size_50(
     #    ╰───| 6 |─╯         |
     #        ─────           |
     #          ╰─────────────╯
+    cur_tile = [0, 1, 2, 0, 3, 0, 4, 5, 0, 6][cur_tile_i]
+    if cur_tile == 0:
+        raise AssertionError("wrong tile")
     match (cur_tile, direction):
         case (1, 3):
             return 6, 0, 0, x + 100
